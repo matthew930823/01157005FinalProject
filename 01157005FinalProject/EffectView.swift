1476305aa9c3903308c207d420b3da82ac212702
Matthew930823 <matthew930823@gmail.com>
2024-12-31 16:21:34 +0800

    check

---
+++ 01157005FinalProject/EffectView.swift
@@ -0,0 +1,18 @@
+//
+//  EffectView.swift
+//  01157005FinalProject
+//
+//  Created by user11 on 2024/11/28.
+//
+
+import SwiftUI
+
+struct EffectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EffectView()
+}
